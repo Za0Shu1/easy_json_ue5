fca0f11e96d50696b6579455ea70f632fa3ce4f4
Za0Shu1 <17781649945@163.com>
2023-02-20 16:46:02 +0800

    Init project

---
+++ Plugins/EasyJson/Source/EasyJson/Private/EasyJson.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EasyJson.h"
+
+#define LOCTEXT_NAMESPACE "FEasyJsonModule"
+
+void FEasyJsonModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FEasyJsonModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FEasyJsonModule, EasyJson)
---
+++ Source/EasyJsonTemplate/EasyJsonTemplateGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EasyJsonTemplateGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EASYJSONTEMPLATE_API AEasyJsonTemplateGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
